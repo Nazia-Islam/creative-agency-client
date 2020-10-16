e8d71d2e69f1b612c0a16d22448d77231dbb6b9c
Nazia-Islam <naziasebelly@gmail.com>
2020-10-16 23:40:59 +0600

    add animation in homepage

--- src/components/Home/Services/ServiceCard.js
+++ src/components/Home/Services/ServiceCard.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useSpring, animated} from 'react-spring';
 
 const ServiceCard = ({service}) => {
     const serviceId = service._id;
     const urlServiceTitle = service.title;
+    const fade = useSpring({
+        from:{
+            opacity: .4
+        },
+        opacity: 1
+    })
+    const fade1 = useSpring({
+        
+        from:{
+            opacity: 0
+        },
+        to:{
+        opacity: 1}
+    })
     return (
-        <div className="col-md-4 service-card">
+        <animated.div style={fade} className="col-md-4 service-card">
             <Link  to={"/service/"+serviceId+"/"+urlServiceTitle}>
                 <img className="w-25 mb-4" src={`data:image/png;base64,${service.image.img}`} alt=""/>
                 <h3 className="text-center" style={{color:"black"}}>{service.title}</h3>
-                <p className="text-center text-secondary">{service.serviceDescription}</p>
+                <animated.p style={fade1} className="text-center text-secondary">{service.serviceDescription}</animated.p>
             </Link>
-        </div>
+        </animated.div>
     );
 };
 
